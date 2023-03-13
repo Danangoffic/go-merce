d817382d20b70f602b4d351e20adaa5db6f29195
Danang Arif Rahmanda <darifrahmanda@gmail.com>
2023-03-14 02:00:19 +0700

    add documentation and waiting process with goroutine in migration

--- app/config/database.go
+++ app/config/database.go
@@ -6,18 +6,25 @@ import (
 	"gorm.io/gorm"
 )
 
+// InitDB function will initialize the database, run the migration table, seed the database if needed
+// and will return as gorm.DB pointer
 func InitDB() *gorm.DB {
-	// Membuat koneksi ke database
+	// create a database connection
 	db, err := setupDatabase()
 	if err != nil {
 		panic("Failed to connect db")
 	}
 
-	// Auto migrate tabel
+	// Auto migrate tabel, wait the progress until done with goroutine
+	go database.Migrate(db)
+
+	//database seeder
 	database.SeedDB(db)
 	return db
 }
 
+// setupDatabase function will create a connection to database, depends on constant.go, you can change the database configuration.
+// This function will return gorm.DB pointer and connection error
 func setupDatabase() (*gorm.DB, error) {
 	db, err := gorm.Open(mysql.New(mysql.Config{
 		DSN: DSN,
