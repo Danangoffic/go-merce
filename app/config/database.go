38ed1980be1ff81b27eb7d09a3ca8734998ce30f
Danang Arif Rahmanda <darifrahmanda@gmail.com>
2023-03-14 03:22:22 +0700

    add databasse config to connection pool

--- app/config/database.go
+++ app/config/database.go
@@ -1,6 +1,8 @@
 package config
 
 import (
+	"time"
+
 	"github.com/Danangoffic/go-merce/app/database"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -11,9 +13,20 @@ import (
 func InitDB() *gorm.DB {
 	// create a database connection
 	db, err := setupDatabase()
+
 	if err != nil {
-		panic("Failed to connect db")
+		panic(err.Error())
 	}
+	sqlDB, err := db.DB()
+
+	// SetMaxIdleConns sets the maximum number of connections in the idle connection pool.
+	sqlDB.SetMaxIdleConns(10)
+
+	// SetMaxOpenConns sets the maximum number of open connections to the database.
+	sqlDB.SetMaxOpenConns(1000)
+
+	// SetConnMaxLifetime sets the maximum amount of time a connection may be reused.
+	sqlDB.SetConnMaxLifetime(time.Hour)
 
 	// Auto migrate tabel, wait the progress until done with goroutine
 	go database.Migrate(db)
