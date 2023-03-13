170bcb32061e7f16715def4ab351d0f663f89df1
Danang Arif Rahmanda <darifrahmanda@gmail.com>
2023-03-14 02:04:59 +0700

    add message package

---
+++ app/utils/Messages/message.go
@@ -0,0 +1,10 @@
+package Messages
+
+// Add message constant here. Exampe: "Success" as success.
+// how it works: Messages.Success
+// Capitalize each initial word
+// set as a string to make it easier to read the messages
+const (
+	// Set a "Success" message as a string.
+	Success string = "Success"
+)
